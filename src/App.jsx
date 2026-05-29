9d0d447da59808d145b8bb64c23620974db432a8
Jiya John <jiyajohn015@gmail.com>
2026-05-29 18:20:26 -0400

    create basic layout

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import UploadPostPage from "./pages/UploadPostPage";
+import EditPostPage from "./pages/EditPostPage";
+import PostDetailPage from "./pages/PostDetailPage";
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/upload" element={<UploadPostPage />} />
+        <Route path="/posts/:id/edit" element={<EditPostPage />} />
+        <Route path="/posts/:id" element={<PostDetailPage />} />
+      </Routes>
+    </Layout>
+    
+  );
+}
+
+export default App
---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Layout({ children }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="logo" onClick={() => navigate("/")}>
+          Taste Trail
+        </div>
+      </header>
+
+      <main className="main">
+        {children}
+      </main>
+    </div>
+  );
+}
